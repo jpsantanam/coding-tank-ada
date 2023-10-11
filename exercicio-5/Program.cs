4b0899c1837566cb875451f8473652a4cec2b8aa
jpsantanam <jpsantanamarques2905@gmail.com>
2023-10-10 21:26:04 -0300

    Adiciona a solução do quinto exercício

---
+++ exercicio-5/Program.cs
@@ -0,0 +1,18 @@
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        for (int i = -5; i <= 7; i += 3)
+        {
+            Console.WriteLine(i);
+        }
+
+        /* 
+        * Resposta: E) -5, -2, 1, 4, 7
+        * Itera de -5 até 7, incrementando 3 no contador a cada iteração
+        * Imprime o valor atual do contador a cada iteração
+        */
+
+    }
+}
